be47db29a6e3bf1e970913b3ef488e5325d59cf9
Mohamamd Ghanem <mohmmad2Ghanem@hotmail.com>
2021-12-19 23:58:02 +0200

    inital ui.

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->middleware(['auth'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/post', function () {
+    return view('post');
+})->middleware(['auth'])->name('post');
 require __DIR__.'/auth.php';
